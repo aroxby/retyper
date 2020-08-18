229e90ec2a0c98e7c87b09060f7a2dd6b68ba4ee
Andy Roxby <aroxby@users.noreply.github.com>
2020-08-18 15:18:09 -0400

    Enable modifer keys

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void sendKeyInput(WORD vk, bool up) {
 	INPUT input = {0};
 	input.type = INPUT_KEYBOARD;
-	input.ki.wVk = vk;
+	input.ki.wVk = vk & 0xFF;
 	input.ki.dwFlags = up ? KEYEVENTF_KEYUP: 0;
 	UINT result = SendInput(1, &input, sizeof(input));
 	if(result != 1) {
@@ -14,21 +14,38 @@ void sendKeyInput(WORD vk, bool up) {
 	}
 }
 
+void sendKeyModifers(WORD vk, bool up) {
+	const static int SHIFT = 1 << 8;
+	const static int CTRL = 1 << 9;
+	const static int ALT = 1 << 10;
+	if(vk & SHIFT) {
+		sendKeyInput(VK_SHIFT, up);
+	}
+	if(vk & CTRL) {
+		sendKeyInput(VK_CONTROL, up);
+	}
+	if(vk & ALT) {
+		sendKeyInput(VK_MENU, up);
+	}
+}
+
 WORD charToVk(char ch) {
-	// TODO: Does this work with modifers like ctrl?
 	SHORT vk = VkKeyScanEx(ch, nullptr);
 	return vk;
 }
 
 void typeChar(char ch) {
-	int KEYS_PER_SEC = 10;
-	sendKeyInput(charToVk(ch), false);
+	const static int KEYS_PER_SEC = 25;
+	WORD vk = charToVk(ch);
+	sendKeyModifers(vk, false);
+	sendKeyInput(vk, false);
 	Sleep(1000 / KEYS_PER_SEC);
-	sendKeyInput(charToVk(ch), true);
+	sendKeyInput(vk, true);
+	sendKeyModifers(vk, true);
 }
 
 int main() {
-	int DELAY = 2;
+	const static int DELAY = 2;
 	Sleep(DELAY * 1000);
 
 	for(;;) {
